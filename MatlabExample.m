28233ca1e3ed6a66a317a1784e82d11be9298683
Alexander Hagg <alexander.hagg@smail.inf.h-brs.de>
2016-05-10 01:39:01 +0200

    Added full HUD option for TORCS sensors

--- MatlabExample.m
+++ MatlabExample.m
@@ -6,10 +6,12 @@ TorcsConfigBase = '/home/alex/torcs-matlab/configs/'; % just insert your config 
 %% Initialization
 killTorcs       = 'killall torcs ';
 killBin         = 'killall torcs-bin ';
-gui             = 0;                        % TORCS gui
+gui             = 1;                        % TORCS gui
+runstats        = 1;                        % RUN statistics
+statframe       = 10;                        % Show run statistics every n frame
 debug           = 0;                        % DEBUG mode
-steps           = 100;                      % Max. simulation time steps (!!)
-data            = zeros(33,steps);          % Will contain all sensor data
+steps           = 1000;                      % Max. simulation time steps (!!)
+data            = zeros(33,1);          % Will contain all sensor data
 
 %% Start the Torcs server
 system(killTorcs);
@@ -17,7 +19,7 @@ system(killBin);
 if gui == 1
     startServer = 'torcs &';
     system(startServer);
-    pause(10);
+    pause(12);
 else
     startServer = ['torcs -t 1000000 -r ' TorcsConfigBase '3001.xml &'];
     disp(startServer)
@@ -28,8 +30,6 @@ end
 
 %% Start the visualization
 figure(1);
-title('Current performance');
-drawnow;
 
 %% Start the client
 c = udp('127.0.0.1', 3001);
@@ -46,7 +46,7 @@ for i=1:steps
         % Read the sensor and other data from the UDP port
         s = sscanf(dat,'(angle %f)(curLapTime %f)(damage %f)(distFromStart %f)(distRaced %f)(fuel %f)(gear %f)(lastLapTime %f)(racePos %f)(rpm %f)(speedX %f)(speedY %f)(speedZ %f)(track %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f)(trackPos %f)');
         sizeS = (size(s));
-        if(sizeS(1) == 33)
+        if(sizeS(1) == 33 && mod(i,10) == 0)
             angle=(s(1)+pi);
             curLapTime=s(2);
             damage=s(3);
@@ -63,20 +63,42 @@ for i=1:steps
             trackSensors=s(14:32);
             trackPos=(s(33)+1);
             
-            % Save the data 
-            data(:,i) = s;
+            % Save the data
+            data(:,end+1) = s;
             % Text display
-            disp(dat)
+            %disp(dat)
             % Plot the data (Please label the plot yourself)
-            plot(data');
-            % Make sure to force drawing, otherwise the plot will only
-            % update after the run. If you are using the TORCS gui, the
-            % plot is not synchronized to the run.
-            %
-            % You might also want to get rid of the empty data columns
-            drawnow;
+            if runstats == 1 && mod(i,statframe) == 0
+                subplot(2,2,1);
+                plot(data([2,4,5,8],:)');
+                legend('Current Lap Time', 'Dist. from Start', 'Dist. Raced', 'Last Lap Time', 'Location', 'NorthEast');
+                title(['Current Position: ' num2str(data(9,end)')]);
+                
+                subplot(2,2,2);
+                plot(data([3,6],:)');
+                title(['Gear: ' num2str(data(7,end)')]);
+                legend('Damage', 'Fuel', 'Location', 'NorthEast');                
+                
+                subplot(2,2,3);
+                semilogy(data([1,10,11,12,13],:)');
+                legend('Angle', 'RPM', 'Speed X', 'Speed Y', 'Speed Z', 'Location', 'SouthEast');
+                
+                subplot(2,2,4);
+                plot(data(33,:)');
+                legend('Track Position', 'Location', 'SouthEast');
+                
+                %legend('Angle', 'Current Lap Time', 'Damage', 'Dist. from Start', 'Dist. Raced', 'Fuel', ...
+                %'Gear', 'Last Lap Time', 'Race Position', 'RPM', 'Speed X', 'Speed Y', 'Speed Z', 'Track Sensors', 'Track Position');
+                
+                % Make sure to force drawing, otherwise the plot will only
+                % update after the run. If you are using the TORCS gui, the
+                % plot is not synchronized to the run.
+                %
+                % You might also want to get rid of the empty data columns
+                drawnow;
+            end
         else
-            disp('readError');
+            %disp('readError (which you can probably ignore)');
         end
     end
 end
